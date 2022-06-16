b8e1ec40e79db28e8030d988d40174498a41ca96
Pablo B. Martinez Pedroso <pmartinezpedroso@gmail.com>
2022-06-16 15:21:17 -0400

    Add Polly for resiliency

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -6,18 +6,32 @@ using Domain.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Polly;
+using Polly.Extensions.Http;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
+using Humanizer;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var policy = HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .WaitAndRetryAsync(3, retryAttempt =>
+    {
+        var nextTimeInterval = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
+        Console.WriteLine($"Polly: error during call, retry for {retryAttempt} time in {TimeSpan.FromSeconds(nextTimeInterval.TotalSeconds).Humanize()} second(s).");
+        return nextTimeInterval;
+    });
 
 var apiBaseUrl = builder.Configuration[AppSettings.ApiBaseUrl] ?? "";
 builder.Services.AddHttpClient(AppSettings.HttpClientApi, (services, client) =>
 {
     client.BaseAddress = new Uri(apiBaseUrl);
     client.EnableIntercept(services);
-});
+})
+    .AddPolicyHandler(policy);
+
 
 builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(AppSettings.HttpClientApi));
 
